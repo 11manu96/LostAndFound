22be06fd8496f59381b9137e8ac0aaacad009110
Manu Maheshwari <manumaheshwari@Manus-MacBook-Pro.local>
2016-04-14 00:10:58 +0530

    Initial commit

---
+++ app/src/main/java/com/example/manumaheshwari/myapplication/ThreeFragment.java
@@ -0,0 +1,29 @@
+package com.example.manumaheshwari.myapplication;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by manumaheshwari on 4/14/16.
+ */
+public class ThreeFragment  extends Fragment {
+
+    public ThreeFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.three_fragment, container, false);
+    }
+}
---
+++ app/src/main/java/com/example/manumaheshwari/myapplication/TwoFragment.java
@@ -0,0 +1,29 @@
+package com.example.manumaheshwari.myapplication;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by manumaheshwari on 4/13/16.
+ */
+public class TwoFragment extends Fragment {
+
+    public TwoFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.two_fragment, container, false);
+    }
+}
